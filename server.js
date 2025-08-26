98579d683dc9623f327ec9a09b45b7911e3f1361
AKSHIT <akshitpaliwal18@gmail.com>
2025-08-26 09:24:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,15 +2,13 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-// Chuck Norris API
 const API_URL = "https://api.chucknorris.io/jokes/random";
 
-// Node.js has fetch built-in (>= v18), no need for axios
 async function getJoke() {
   try {
     const response = await fetch(API_URL);
     const data = await response.json();
-    return data.value; // joke string
+    return data.value; 
   } catch (err) {
     return "Error fetching joke 🤕";
   }
@@ -18,7 +16,6 @@ async function getJoke() {
 
 const server = http.createServer(async (req, res) => {
   if (req.url === "/" || req.url === "/index.html") {
-    // Serve frontend
     fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
       if (err) {
         res.writeHead(500);
@@ -29,7 +26,6 @@ const server = http.createServer(async (req, res) => {
       }
     });
   } else if (req.url === "/style.css") {
-    // Serve CSS
     fs.readFile(path.join(__dirname, "style.css"), (err, data) => {
       if (err) {
         res.writeHead(500);
@@ -40,7 +36,6 @@ const server = http.createServer(async (req, res) => {
       }
     });
   } else if (req.url === "/api/joke") {
-    // Serve joke from external API
     const joke = await getJoke();
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ joke }));
@@ -53,3 +48,4 @@ const server = http.createServer(async (req, res) => {
 server.listen(3000, () => {
   console.log("Server running at http://localhost:3000");
 });
+
